3290dd035729bb0fb4fbce201c1df2c32ac3a667
Akhil Sharma <akhil.console@gmail.com>
2021-02-17 22:43:36 +0530

    Working properly and giving proper result

---
+++ src/apnikaksha/SeriesSum.java
@@ -0,0 +1,23 @@
+package apnikaksha;
+
+import java.util.Scanner;
+
+public class SeriesSum {
+    public static void main(String[] args) {
+
+/*    We have to print the value of
+    1 + 1/2 + 1/3 + 1/4 + 1/5 + 1/6 + 1/7 + 1/8...n  */
+
+        Scanner scanner = new Scanner(System.in);
+        int n  = scanner.nextInt();
+
+        double result = 0 ;
+
+        for (float i = 1; i < n ; i++){
+            double x =  1/i;
+            result += x;
+
+        }
+        System.out.println(result + " re");
+    }
+}
