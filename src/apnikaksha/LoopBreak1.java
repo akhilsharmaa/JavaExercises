9fd3fab817848ff0a4685a27f67b2212d73a2d2c
Akhil Sharma <akhil.console@gmail.com>
2021-02-18 07:19:04 +0530

    Working properly Take user input until user give the number.

---
+++ src/apnikaksha/LoopBreak1.java
@@ -0,0 +1,23 @@
+package apnikaksha;
+
+import java.util.Scanner;
+
+public class LoopBreak1 {
+    public static void main(String[] args) {
+
+/*  Take user input until user give the Negative number */
+
+        Scanner scanner = new Scanner(System.in);
+
+       while (true){
+           System.out.println("Enter the Negative number please");
+           int inputNum = scanner.nextInt();
+
+           if (inputNum <= 0 ){
+               break;
+           }else continue;
+       }
+        System.out.println("Thanks your negative number");
+
+    }
+}
--- src/apnikaksha/SeriesSum2.java
+++ src/apnikaksha/SeriesSum2.java
@@ -6,6 +6,7 @@ public class SeriesSum2 {
 
  /*    We have to print the value of
     1 - 1/2 + 1/3 - 1/4 + 1/5 - 1/6 + 1/7 - 1/8...n  */
+
         Scanner scanner = new Scanner(System.in);
         int n  = scanner.nextInt();
 
